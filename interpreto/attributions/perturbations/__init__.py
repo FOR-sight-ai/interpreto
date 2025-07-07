03ea9c0c0851aba71ef979d58dba286b65d6672a
cofri <cofri@users.noreply.github.com>
2025-07-07 17:59:58 +0200

    feat: introduce InsertionPerturbator and DeletionPerturbator
    These new classes are the perturbator for Deletion and Insertion metrics. It
    perturbs the inputs based on the explanations previously computed.

--- interpreto/attributions/perturbations/__init__.py
+++ interpreto/attributions/perturbations/__init__.py
@@ -25,6 +25,7 @@
 from .base import IdsPerturbator
 from .gaussian_noise_perturbation import GaussianNoisePerturbator
 from .gradient_shap_perturbation import GradientShapPerturbator
+from .insertion_deletion_perturbation import DeletionPerturbator, InsertionPerturbator
 from .linear_interpolation_perturbation import LinearInterpolationPerturbator
 from .occlusion_perturbation import OcclusionPerturbator
 from .random_perturbation import RandomMaskedTokenPerturbator
