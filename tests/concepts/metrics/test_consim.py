db06cc5d5588878604daf9ddc8ef9cfa88035175
Antonin Poché <antonin.poche@irt-saintexupery.com>
2025-08-13 10:32:45 +0200

    consim: linting

--- tests/concepts/metrics/test_consim.py
+++ tests/concepts/metrics/test_consim.py
@@ -594,8 +594,8 @@ def test_consim_evaluate_with_openai(splitted_encoder_ml: ModelWithSplitPoints):
     """
     # lazy import to avoid importing openai
     from interpreto.model_wrapping.llm_interface import (
-        OpenAILLM,  # noqa: PLC0415  # ruff: disable=import-outside-toplevel
-    )
+        OpenAILLM,
+    )  # noqa: PLC0415  # ruff: disable=import-outside-toplevel
 
     open_ai_llm = OpenAILLM(api_key=os.environ["OPENAI_API_KEY"], model="gpt-4o-mini-2024-07-18")
 
