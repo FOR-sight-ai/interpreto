55386b677a24fcb4a544d69b9ae7b3e747b4c62a
Hugo De Bosschere <hugo.debosschere-work@proton.me>
2026-08-01 18:37:03 +0200

    bugs in the tests

--- tests/attributions/methods/test_merge_attribution_methods_vision.py
+++ tests/attributions/methods/test_merge_attribution_methods_vision.py
@@ -32,11 +32,11 @@ from PIL import Image
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 from transformers.image_processing_utils import BatchFeature
 
-from interpreto.attribution.perturbations.smoothgrad_merged import SmoothGrad
 from interpreto.attributions.aggregations.base import MeanAggregator, OcclusionAggregator
 from interpreto.attributions.base_merged import ImageAttributionOutput
 from interpreto.attributions.methods.occlusion_merge import Occlusion
 from interpreto.attributions.perturbations.occlusion_perturbation_merged import OcclusionPerturbator
+from interpreto.attributions.perturbations.smoothgrad_merged import SmoothGrad
 from interpreto.commons.granularity import GranularityResizeStrategy, ImageGranularity
 from interpreto.visualizations.image_attributions_merge import plot_image_attribution
 
